0163292b1bc77adf0ed7ac3de2ad71a9e1fc94a6
amellapower <39852391+amellapower@users.noreply.github.com>
2022-12-01 18:12:07 +0900

    MakeCode 프로젝트용 기초 파일

---
+++ main.ts
@@ -0,0 +1,171 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(60)
+    basic.pause(5000)
+    basic.showNumber(59)
+    basic.pause(5000)
+    basic.showNumber(58)
+    basic.pause(5000)
+    basic.showNumber(57)
+    basic.pause(5000)
+    basic.showNumber(56)
+    basic.pause(5000)
+    basic.showNumber(55)
+    basic.pause(5000)
+    basic.showNumber(54)
+    basic.pause(5000)
+    basic.showNumber(53)
+    basic.pause(5000)
+    basic.showNumber(52)
+    basic.pause(5000)
+    basic.showNumber(51)
+    basic.pause(5000)
+    basic.showNumber(50)
+    basic.pause(5000)
+    basic.showNumber(49)
+    basic.pause(5000)
+    basic.showNumber(48)
+    basic.pause(5000)
+    basic.showNumber(47)
+    basic.pause(5000)
+    basic.showNumber(46)
+    basic.pause(5000)
+    basic.showNumber(45)
+    basic.pause(5000)
+    basic.showNumber(44)
+    basic.pause(5000)
+    basic.showNumber(43)
+    basic.pause(5000)
+    basic.showNumber(42)
+    basic.pause(5000)
+    basic.showNumber(41)
+    basic.pause(5000)
+    basic.showNumber(40)
+    basic.pause(5000)
+    basic.showNumber(39)
+    basic.pause(5000)
+    basic.showNumber(38)
+    basic.pause(5000)
+    basic.showNumber(37)
+    basic.pause(5000)
+    basic.showNumber(36)
+    basic.pause(5000)
+    basic.showNumber(35)
+    basic.pause(5000)
+    basic.showNumber(34)
+    basic.pause(5000)
+    basic.showNumber(33)
+    basic.pause(5000)
+    basic.showNumber(31)
+    basic.pause(5000)
+    basic.showNumber(32)
+    basic.pause(5000)
+    basic.showNumber(30)
+    basic.pause(5000)
+    basic.showNumber(29)
+    basic.pause(5000)
+    basic.showNumber(28)
+    basic.pause(5000)
+    basic.showNumber(27)
+    basic.pause(5000)
+    basic.showNumber(26)
+    basic.pause(5000)
+    basic.showNumber(25)
+    basic.pause(5000)
+    basic.showNumber(24)
+    basic.pause(5000)
+    basic.showNumber(23)
+    basic.pause(5000)
+    basic.showNumber(22)
+    basic.pause(5000)
+    basic.showNumber(21)
+    basic.pause(5000)
+    basic.showNumber(20)
+    basic.pause(5000)
+    basic.showNumber(19)
+    basic.pause(5000)
+    basic.showNumber(18)
+    basic.pause(5000)
+    basic.showNumber(17)
+    basic.pause(5000)
+    basic.showNumber(16)
+    basic.pause(5000)
+    basic.showNumber(15)
+    basic.pause(5000)
+    basic.showNumber(14)
+    basic.pause(5000)
+    basic.showNumber(13)
+    basic.pause(5000)
+    basic.showNumber(12)
+    basic.pause(5000)
+    basic.showNumber(11)
+    basic.pause(5000)
+    basic.showNumber(10)
+    basic.pause(5000)
+    basic.showNumber(9)
+    basic.pause(5000)
+    basic.showNumber(8)
+    basic.pause(5000)
+    basic.showNumber(7)
+    basic.pause(5000)
+    basic.showNumber(6)
+    basic.pause(5000)
+    basic.showNumber(5)
+    basic.pause(5000)
+    basic.showNumber(4)
+    basic.pause(5000)
+    basic.showNumber(3)
+    basic.pause(5000)
+    basic.showNumber(2)
+    basic.pause(5000)
+    basic.showNumber(1)
+    basic.pause(5000)
+    basic.showNumber(0)
+    basic.pause(5000)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . # # # .
+        . # # # .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . # # # .
+        . # # # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        # . # . #
+        . # # # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        # # # # #
+        . . # . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . # # # .
+        # . # . #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . # # # .
+        . # # # .
+        `)
+})
+basic.forever(function () {
+	
+})
